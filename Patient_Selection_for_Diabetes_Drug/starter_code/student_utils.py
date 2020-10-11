f5370d51ee1610ef95dd006c008480cc1939127b
PFGR <pfgr@live.com>
2020-10-11 17:31:08 -0400

    Corrected typo

--- Patient_Selection_for_Diabetes_Drug/starter_code/student_utils.py
+++ Patient_Selection_for_Diabetes_Drug/starter_code/student_utils.py
@@ -13,7 +13,7 @@ def reduce_dimension_ndc(df, ndc_df):
         df: pandas dataframe, output dataframe with joined generic drug name
     '''
     # dict of codes to generic drug name
-    code_drug = dict(zip(df.ndc_code, ndc_df['Non-proprietary Name']))
+    code_drug = dict(zip(ndc_df.ndc_code, ndc_df['Non-proprietary Name']))
     # the nan values was given a random drug equivalent
     code_drug[np.nan] = np.nan
     # new column for drug names and map codes to drug names
